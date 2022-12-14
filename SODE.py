b9ec79b2a00c3b0ddec4357520bd370fc4173229
Stas Indyukov <stasun008@gmail.com>
2022-12-15 01:15:24 +0300

    yes

--- SODE.py
+++ SODE.py
@@ -5,28 +5,30 @@ import matplotlib.pyplot as plt
 from NLAE import non_linear_solve as n_solve
 
 
-# def du1dt(u1: float, u2: float, t: float, a: float):
-#     if t == 0:
-#         return -u1 * u2 + 1.
-#
-#     return -u1 * u2 + np.sin(t) / t
-#
-#
-# def du2dt(u1: float, u2: float, t: float, a: float):
-#     return -(u2 * u2) + a * t / (1 + t * t)
-# def a_param(w: float):
-#     return 2.5 + w / 40.
-
 def du1dt(u1: float, u2: float, t: float, a: float):
-    return u2 - (a * u1 + 0.25 * u2) * u1
+    if t == 0:
+        return -u1 * u2 + 1.
+
+    return -u1 * u2 + np.sin(t) / t
 
 
 def du2dt(u1: float, u2: float, t: float, a: float):
-    return np.exp(u1) - (u1 + a * u2) * u1
+    return -(u2 * u2) + a * t / (1 + t * t)
 
 
 def a_param(w: float):
-    return 2
+    return 2.5 + w / 40.
+
+# def du1dt(u1: float, u2: float, t: float, a: float):
+#     return u2 - (a * u1 + 0.25 * u2) * u1
+#
+#
+# def du2dt(u1: float, u2: float, t: float, a: float):
+#     return np.exp(u1) - (u1 + a * u2) * u1
+#
+#
+# def a_param(w: float):
+#     return 2
 
 
 def explicit_euler_method(*funcs: Callable, u0: np.array, t_upper: float, E: float, tau_max: float):
@@ -71,7 +73,7 @@ def equation(var, *data):
     u1, u2 = var
     eq1 = du1dt(u1, u2, t, a)
     eq2 = du2dt(u1, u2, t, a)
-    return y_next - y - np.array([eq1, eq2], dtype=float)
+    return y_next - y - tau * np.array([eq1, eq2], dtype=float)
 
 
 def implicit_euler_method(*funcs: Callable, u0: np.array, t_upper: float, E: float, tau_min: float, tau_max: float):
@@ -93,15 +95,15 @@ def implicit_euler_method(*funcs: Callable, u0: np.array, t_upper: float, E: flo
         while True:
             t_next = t + tau
 
-            # y_next = fsolve(equation, y_next, args=(t_next, a_param(40), tau, y, y_next))
+            y_next = fsolve(equation, y_next, args=(t_next, a_param(40), tau, y, y_next))
 
-            gen = n_solve(u0, 3, 0.3, 0.01, du1dt, du2dt, max_iters=100, args=(t_next, a_param(25)))
-            while True:
-                try:
-                    y_next, iters = next(gen)
-                    # print(f'\t{iters}: ' + ' '.join([str(i) for i in x]))
-                except StopIteration:
-                    break
+            # gen = n_solve(u0, 1e-5, 1e-5, 0.01, du1dt, du2dt, max_iters=100, args=(t_next, a_param(25)))
+            # while True:
+            #     try:
+            #         y_next, iters = next(gen)
+            #         # print(f'\t{iters}: ' + ' '.join([str(i) for i in x]))
+            #     except StopIteration:
+            #         break
 
             Ek = np.max(np.abs(-1 * tau / (tau + tau_prev) * (y_next - y - tau / tau_prev * (y - y_prev))))
 
@@ -139,8 +141,8 @@ def implicit_euler_method(*funcs: Callable, u0: np.array, t_upper: float, E: flo
 
 
 def main():
-    # u0 = np.array([0., -0.412])
-    u0 = np.array([1, 0.01])
+    u0 = np.array([0., -0.412])
+    #u0 = np.array([1, 0.01])
 
     t = 1
     E = 10 ** -1
